ff80a5798e05dbad9d517649d367d3a6e0240bf7
Ruan Silva <ruanluiz.as@gmail.com>
2019-01-13 19:08:52 -0200

    modulo 2 - aula 2

--- app/src/main/java/com/example/ruan/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/example/ruan/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.example.ruan.aluraviagens.R;
 import com.example.ruan.aluraviagens.model.Pacote;
+import com.example.ruan.aluraviagens.util.DataUtil;
 import com.example.ruan.aluraviagens.util.DiasUtil;
 import com.example.ruan.aluraviagens.util.MoedaUtil;
 import com.example.ruan.aluraviagens.util.ResourceUtil;
@@ -16,27 +17,51 @@ import java.math.BigDecimal;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Resumo do pacote";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
 
+        setTitle(TITULO_APPBAR);
+
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
                 2, new BigDecimal(243.99));
 
-        TextView local = findViewById(R.id.resumo_pacote_local);
-        local.setText(pacoteSaoPaulo.getLocal());
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraDias(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+    }
 
-        ImageView image = findViewById(R.id.resumo_pacote_imagem);
-        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
-        image.setImageDrawable(drawableDoPacote);
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto( pacote.getDias() );
+        data.setText(dataFormatadaDaViagem);
+    }
 
-        TextView dias = findViewById(R.id.resumo_pacote_dias);
-        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
-        dias.setText(diasEmTexto);
-
+    private void mostraPreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_pacote_preco);
-        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
         preco.setText(moedaBrasileira);
+    }
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView image = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacote.getImagem());
+        image.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacote.getLocal());
     }
 }
